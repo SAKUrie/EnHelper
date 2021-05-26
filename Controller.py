a88cfb1d200f139231788ba90794d62d29d423c1
SAKUrie <2309233790@qq.com>
2021-05-26 14:46:19 +0800

    5.26 完成主界面界面显示与相关槽函数 #todo 数据库信息导入 数据查询界面与槽函数编写 背诵界面

--- Controller.py
+++ Controller.py
@@ -1,5 +1,8 @@
 import LoginUI,MainUI,MemoryUI,SearchUI,TestUI
 from PyQt5.QtWidgets import QGraphicsObject
+
+userid_now="0000001"
+
 class controller(QGraphicsObject):
     def init(self):
         super().__init__()
